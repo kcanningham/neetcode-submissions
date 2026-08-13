0013fd7dde0e3a8e9b57f486688e71ee8311dc68
Kanishk Verma <71752524+kcanningham@users.noreply.github.com>
2026-08-13 15:58:08 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool check(int m, vector<int>& piles, int h) {
+        double time = 0;
+        for(int i = 0; i < piles.size(); i++) {
+            time += (piles[i] + m - 1)/m;
+        }
+        if(time <= h) return true;
+        else return false;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(), piles.end());
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int min_ans = high;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(check(mid, piles, h)) {
+                min_ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return min_ans;
+    }
+};
